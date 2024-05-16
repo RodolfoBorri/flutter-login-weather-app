cbedb62d738b9ee8f782bf72fad3c6ca97a3ed37
RodolfoBorri <rodolfoborri@hotmail.com>
2024-05-16 16:52:52 -0300

    Adding login tweaks and login-icon

--- flutter_login_weather_app/lib/login_page.dart
+++ flutter_login_weather_app/lib/login_page.dart
@@ -13,14 +13,47 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {    
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/login-background.jpg"),
             fit: BoxFit.cover,
           ),
         ),
-        child: null /* add child content here */,
-      )
+        child: Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height/1.2,
+              width: MediaQuery.of(context).size.width/3,              
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.white,
+              ),
+              child: Form(
+                child: Column(
+                  children: [
+                    Column(
+                      children: <Widget> [
+                        Image.asset(
+                          'assets/images/weather-login-icon.jpg',
+                          height: 150,
+                          fit:BoxFit.fill
+                        ),
+                        Text(
+                          'Weather App',
+                          style: TextStyle(
+                            height: 2, 
+                            fontSize: 50,
+                            color: const Color.fromARGB(255, 1, 39, 104),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),                    
+                  ],
+                ),                
+              ),
+            ),
+        ),
+      ),
     );
-  }  
+  }   
 }
