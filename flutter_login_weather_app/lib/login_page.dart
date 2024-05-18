8f0e1b5c7ae4735702b1108406727e02421d460f
RodolfoBorri <rodolfoborri@hotmail.com>
2024-05-18 18:36:19 -0300

    Changed background image, added username and password fields, added toggle on password 'obscureText' property

--- flutter_login_weather_app/lib/login_page.dart
+++ flutter_login_weather_app/lib/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,6 +11,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   
+  bool _isHidden = true;
+
   @override
   Widget build(BuildContext context) {    
     return Scaffold(
@@ -20,40 +24,139 @@ class _LoginPageState extends State<LoginPage> {
           ),
         ),
         child: Center(
-            child: Container(
-              height: MediaQuery.of(context).size.height/1.2,
-              width: MediaQuery.of(context).size.width/3,              
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.white,
-              ),
-              child: Form(
-                child: Column(
-                  children: [
-                    Column(
-                      children: <Widget> [
-                        Image.asset(
-                          'assets/images/weather-login-icon.jpg',
-                          height: 150,
-                          fit:BoxFit.fill
-                        ),
-                        Text(
-                          'Weather App',
-                          style: TextStyle(
-                            height: 2, 
-                            fontSize: 50,
-                            color: const Color.fromARGB(255, 1, 39, 104),
+          child: ClipRect(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+              child: Container(
+                height: MediaQuery.of(context).size.height/1.2,
+                width: MediaQuery.of(context).size.width/3,              
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  color: Colors.white.withOpacity(0.4),
+                ),
+                child: Form(
+                  child: Column(
+                    children: [
+                      Column(
+                        children: <Widget> [
+                          Image.asset(
+                            'assets/images/weather-login-icon.jpg',
+                            height: 150,
+                            fit:BoxFit.fill
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10),                    
-                  ],
-                ),                
+                          const Text(
+                            'Sign In',
+                            style: TextStyle(
+                              height: 2, 
+                              fontSize: 50,
+                              color: Colors.black87,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          SizedBox(width: MediaQuery.of(context).size.width/24),
+                          const Text(
+                            'Username',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black87
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 3),
+                      Row(
+                        children: [
+                          SizedBox(width: MediaQuery.of(context).size.width/24),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width/4,
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.person),
+                                border: OutlineInputBorder(),
+                                hintText: 'Enter your username',
+                                hintStyle: TextStyle(
+                                  color: Colors.black45
+                                )
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          SizedBox(width: MediaQuery.of(context).size.width/24),
+                          const Text(
+                            'Password',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black87
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 3),
+                      Row(
+                        children: [
+                          SizedBox(width: MediaQuery.of(context).size.width/24),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width/4,
+                            child: TextField(
+                              obscureText: _isHidden,
+                              enableSuggestions: false,
+                              autocorrect: false,
+                              decoration: InputDecoration(
+                                prefixIcon: const Icon(Icons.key),
+                                suffix: InkWell(
+                                    onTap: _togglePasswordView,
+                                    child: Icon(
+                                            _isHidden 
+                                            ? Icons.visibility_off
+                                            : Icons.visibility,
+                                          ),
+                                    ),
+                                border: const OutlineInputBorder(),
+                                hintText: 'Enter your password',
+                                hintStyle: const TextStyle(
+                                  color: Colors.black45
+                                )
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 30),
+                      Column(
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width/4 - 5,
+                            height: MediaQuery.of(context).size.width/50,
+                            child: ElevatedButton(
+                              child: const Text('Login'), 
+                              onPressed: () => (),
+                            )
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
+          ),
         ),
       ),
     );
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
   }   
 }
